2d227a5ae25ac2cc1560435eed893385bb01747a
Rob Stevenson-Leggett <r.leggett@building-blocks.com>
2017-04-18 09:38:06 +0100

    adding project

---
+++ GameOfLife.Test/GameOfLifeSpec.fs
@@ -0,0 +1,13 @@
+﻿module ``Describe Game Of Life``
+    open NUnit.Framework
+    open FsUnit
+
+    [<TestFixture>]
+    type ``Given a single tick occurred`` () =
+        let gameOfLife = new GameOfLife()
+
+        [<Test>] member x.
+            ``when there are no living cells, no cells come to life`` () =
+                gameOfLife.Evolve(new Set<Cell>())
+
+    
---
+++ GameOfLife/GameOfLife.fs
@@ -0,0 +1,17 @@
+﻿namespace GameOfLife
+
+open System
+
+type Cell() =
+    let X = 0
+    let Y = 0
+
+    interface IComparable<Cell> with
+        member this.CompareTo(obj: Cell): int = 
+            0
+
+
+type GameOfLife() = 
+    member this.Evolve (set: Set<Cell>) = 
+        null    
+
